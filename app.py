a146b6aec6be010ddb0e1536845ccf710f5fbc9c
giov8 <giovani,.ggm@gmail.com>
2024-10-24 14:00:11 -0300

    Organização pastas

---
+++ app.py
@@ -0,0 +1,13 @@
+# Importando a biblioteca do Flask para fazer um site dinâmico
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Definindo a rota principal do site
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Parte principal do programa em Python
+if __name__ == '__main__':
+    app.run(debug=True)
