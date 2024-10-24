9d4bf6bc69f8fd0cfd893634c5fe8f266956fe3e
giov8 <giovani,.ggm@gmail.com>
2024-10-24 14:38:13 -0300

    criou o servidor

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Importando a biblioteca do Flask para fazer um site dinâmico
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
+# Criando um objeto do servidor
 app = Flask(__name__)
 
 # Definindo a rota principal do site
